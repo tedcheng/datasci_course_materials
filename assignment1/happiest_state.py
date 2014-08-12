8aac3bbe5f10c0087499d149f3f9959197e205ce
Ted Cheng <yiqun.cheng@gmail.com>
2014-08-12 12:13:59 -0700

    add happiest_state solution

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import operator
 
 def build_dict_from_sentiment_file(sent_file):
     scores = {}
@@ -28,44 +29,35 @@ def compute_tweet_sentiment_score(sent_dict, tweet):
             score += word_score
     return score
 
-def count_sentiment_words(sent_dict, tweet):
-    words = split_tweet_into_words(tweet)
-    count = 0
-    for word in words:
-        word_score = sent_dict.get(word)
-        if word_score == None:
-            count += 0
-        else:
-            count += 1
-    return count
-
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    old_dict = build_dict_from_sentiment_file(sent_file)
-    new_dict = {}
+    sent_dict = build_dict_from_sentiment_file(sent_file)
+    state_scores = {}
     for line in tweet_file:
-        tweet = json.loads(line).get("text")
-        if tweet != None:
-            score = compute_tweet_sentiment_score(old_dict, tweet)
-            count = count_sentiment_words(old_dict, tweet)
-            if count != 0:
-              avg_term_score = float(score) / float(count)
-            else:
-              avg_term_score = 0
+        line_object = json.loads(line)
+        tweet = line_object.get("text")
+        place = line_object.get("place")
+        if (tweet != None) and (place != None):
+            country = place.get("country")
+            place_type = place.get("place_type")
+            name = place.get("full_name")
+            if (country == "United States") and (place_type == "city"):
+                score = compute_tweet_sentiment_score(sent_dict, tweet)
+                state = name[-2:]
+                if state_scores.get(state) == None:
+                    state_scores[state] = [score]
+                else:
+                    state_scores[state].append(score)
+    
+    state_score = {}
+    for state, scores in state_scores.items():
+        avg_score = sum(scores)/len(scores)
+        # print state, avg_score
+        state_score[state] = avg_score
 
-            terms = split_tweet_into_words(tweet)
-            for term in terms:
-                if old_dict.get(term) == None:
-                    if new_dict.get(term) == None:
-                        new_dict[term] = [avg_term_score]
-                    else:
-                        new_dict.get(term).append(avg_term_score)
-
-    for term, scores in new_dict.items():        
-        # avg_score = float(sum(scores))/len(scores)
-        # line = term + " " + avg_score 
-        print term, float(sum(scores))/len(scores)
+    happiest_state = sorted(state_score.iteritems(), key=operator.itemgetter(1), reverse=True)[0:1]
+    print happiest_state[0][0]
 
 if __name__ == '__main__':
     main()
